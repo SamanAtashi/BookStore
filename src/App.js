405bec16f3da05e1659e19fda37a4c57983ae1d8
SamanAtashi <milady00126@gmail.com>
2021-09-10 07:54:09 +0300

    Update propTypes + changed routes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from 'react';
 import {
   BrowserRouter as Router, Switch, Route, Link,
 } from 'react-router-dom';
-import Books from './components/Books';
-import Categories from './components/Categories';
-import AddBook from './components/AddBook';
+import Books from './components/Books/Books';
+import Categories from './components/Categories/Categories';
+import AddBook from './components/Books/Book/AddBook';
 
 const App = () => (
   <div>
     <h1>Welcome to BookStore</h1>
     <Router>
-      <Link to="/books">Books</Link>
+      <Link to="/">Books</Link>
       <Link to="/categories">Categories</Link>
       <Switch>
-        <Route path="/books">
-          <Books />
-        </Route>
         <Route path="/categories">
           <Categories />
+        </Route>
+        <Route exact path="/">
+          <Books />
         </Route>
       </Switch>
     </Router>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Book from './Book';
+import Book from './Book/Book';
 
 const Books = () => (
   <ul>
