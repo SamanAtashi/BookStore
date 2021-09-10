cc8279bb7bcb6cfd9f5a2d38473bd117496c46fc
SamanAtashi <milady00126@gmail.com>
2021-09-10 08:17:49 +0300

    Add basic cinfig file for redux

--- src/components/Books/Book/AddBook.jsx
+++ src/components/Books/Book/AddBook.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import uuid from 'react-uuid'
 
-const AddBook = () => (
+uuid()
+
+
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+// import your Action Creators
+import {addBook, removeBook} from '../../../redux/books/books';
+
+const AddBook = () => {
+const dispatch = useDispatch();
+
+const submitBookToStore = () => {
+    const newBook = {
+        id:
+        title,
+        author
+    }
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+}
+
+return (
   <form>
     <input type="text" name="Title" placeholder="Title" />
     <input type="text" name="Author" placeholder="Author" />
-    <input type="button" value="Add" />
-  </form>
-);
+    <input type="button" value="Add" onClick={submitBookToStore} />
+  </form>)
+};
 
 export default AddBook;
+
+
+...
